e74563be1f1e69aa4b6e7341d636828229e65532
Cory Johns <johnsca@gmail.com>
2020-04-20 11:14:38 -0400

    Fix unit test not being run, and having wrong call counts

--- tests/test_keepalived.py
+++ tests/test_keepalived.py
@@ -8,10 +8,10 @@ def test_series_upgrade():
     assert host.service_resume.call_count == 0
     assert hookenv.status_set.call_count == 0
     handlers.pre_series_upgrade()
-    assert host.service_pause.call_count == 1
+    assert host.service_pause.call_count == 2
     assert host.service_resume.call_count == 0
     assert hookenv.status_set.call_count == 1
     handlers.post_series_upgrade()
-    assert host.service_pause.call_count == 1
-    assert host.service_resume.call_count == 1
+    assert host.service_pause.call_count == 2
+    assert host.service_resume.call_count == 2
     assert hookenv.status_set.call_count == 1
